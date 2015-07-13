37db5072eb56c22ce60363ede9e38b99a34371a2
Guido Pardo <guidopardo@gmail.com>
2015-07-13 12:40:32 -0300

    Cuando la cantidad de productos es mayor a la de stock y se toca cancelar en el cartel, ahora desaparece

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -20,5 +20,5 @@ function checkMoverCarrito(idNameArticulo){
 }//End function checkMoverCarrito
 
 function cancelaMoverCarrito(){
-	
+	$("#msjError00").addClass("hidden");
 }//End function cancelaMoverCarrito
